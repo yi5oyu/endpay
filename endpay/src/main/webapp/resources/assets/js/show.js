51c37bf2750d82d57623bb52413145f332c18e1e
yi5oyu <yi5oyu@gmail.com>
2024-02-01 02:21:05 +0900

    show air datepicker js / data_box css 수정

--- endpay/src/main/webapp/resources/assets/js/show.js
+++ endpay/src/main/webapp/resources/assets/js/show.js
@@ -21,16 +21,15 @@
 	    range: true,
 	    multipleDatesSeparator: ' ~ ',
 	    language: {
-	        daysMin: ['일', '월', '화', '수', '목', '금', '토'],
-	        months: [
-	            '1월', '2월', '3월', '4월', '5월', '6월',
-	            '7월', '8월', '9월', '10월', '11월', '12월'
-	        ],
-	        today: '오늘',
-	        clear: '지우기',
-	        dateFormat: 'yyyy-mm-dd',
-	        firstDay: 0
+            daysMin: ['일', '월', '화', '수', '목', '금', '토'],
+            months: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+            monthsShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+            dateFormat: 'yyyy-mm-dd',
+            firstDay: 0,
 	    },
+	    navTitles: {
+            days: '<h1>yyyy</h1>,' + '<h1> &nbsp;MM<h1>'
+        },
 	    MaxDate: new Date(),
 	    dateFormat: 'yyyy-mm-dd', 
 	    onSelect: function (formattedDate, date, picker) {
